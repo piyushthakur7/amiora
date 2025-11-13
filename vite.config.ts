86f2472af58b78976484f638f2f16856657f9bae
Piyush <tpiyush300@gmail.com>
2025-11-13 19:08:08 +0530

    vite update

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import path from "path"
+import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal"
 
 export default defineConfig({
   plugins: [
     react(),
-    runtimeErrorOverlay(),
-    ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
-      ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
-          ),
-          await import("@replit/vite-plugin-dev-banner").then((m) =>
-            m.devBanner(),
-          ),
-        ]
-      : []),
+    runtimeErrorOverlay()
   ],
   resolve: {
     alias: {
@@ -28,8 +17,8 @@ export default defineConfig({
   },
   root: path.resolve(import.meta.dirname, "client"),
   build: {
-    outDir: path.resolve(import.meta.dirname, "dist/public"),
-    emptyOutDir: true,
+    outDir: "dist",   // <-- FIXED
+    emptyOutDir: true
   },
   server: {
     fs: {
@@ -37,4 +26,4 @@ export default defineConfig({
       deny: ["**/.*"],
     },
   },
-});
+})
